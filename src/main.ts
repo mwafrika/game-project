404cfaecadc3bc4816501d1c553f019fa5bf2a23
github <25848053+mwafrika@users.noreply.github.com>
2021-11-06 15:50:45 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+   app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   const config = new DocumentBuilder()
     .setTitle('Game project')
     .setDescription('Test for the game project')
